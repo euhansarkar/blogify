97adee1194bd26dee19e138c2cd4072edc62e8ac
euhansarkar <euhansarkar@gmail.com>
2023-03-27 03:47:04 +0600

    dark mode completed

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,15 +1,49 @@
 import Link from "next/link";
+import { BsMoonStarsFill, BsSun } from "react-icons/bs";
 import { ImFacebook, ImTwitter, ImYoutube } from "react-icons/im";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
 const Header = () => {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, [])
+
+  const renderThemeChanger = () => {
+    if(!mounted) return null;
+    const currentTheme = theme === `system` ? systemTheme : theme;
+
+    if (currentTheme === `dark`) {
+      return (
+        <BsSun
+          className="w-7 h-7"
+          role="button"
+          onClick={() => setTheme(`light`)}
+        />
+      );
+    } else {
+      return (
+        <BsMoonStarsFill
+          className="w-7 h-7"
+          role="button"
+          onClick={() => setTheme(`dark`)}
+        />
+      );
+    }
+  };
+
   return (
-    <header className="bg-gray-500">
+    <header className="bg-gray-500 dark:border-gray-700">
       <div className="xl:container xl:mx-auto flex flex-col items-center sm:flex-row justify-between text-center p-4">
         <div className="md:flex-none w-96 order-2 sm:order-1 flex justify-center py-4 sm:py-0">
           <input type="text" className="input-text" placeholder="search" />
         </div>
         <div className="shrink w-80 sm:order-2">
-          <Link href={`/`}className="font-bold uppercase text-3xl">
-              design
+          <Link href={`/`} className="font-bold uppercase text-3xl">
+            design
           </Link>
         </div>
         <div className="flex w-96 order-3 justify-center gap-6">
@@ -27,6 +61,9 @@ const Header = () => {
             <Link href={`/`}>
               <ImYoutube color="888888" />
             </Link>
+          </div>
+          <div>
+            {renderThemeChanger()}
           </div>
         </div>
       </div>
